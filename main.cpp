71f80fbce0c50447fc809ca0abdc7cc72c956939
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-07-07 21:44:22 +0900

    add create post api

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 #include <sstream>
 #include <string>
 
+struct Post{
+    int id;
+    std::string content;
+};
+
+using namespace sqlite_orm;
+
 const std::string load_static(const std::string& path) {
 
     std::ifstream static_file(path.c_str(), std::ios::in);
@@ -20,6 +27,11 @@ int main() {
 
     httplib::Server svr;
 
+    auto storage = make_storage("db.sqlite",
+                            make_table("posts",
+                                       make_column("id", &Post::id, autoincrement(), primary_key()),
+                                       make_column("content", &Post::content));
+
     const std::string html = load_static("static/index.html"); 
 
     const std::string js = load_static("static/index.js"); 
@@ -31,6 +43,12 @@ int main() {
     svr.Get("/index.js", [&](const httplib::Request& req, httplib::Response& res){
         res.set_content(js, "text/javascript");
     });
+
+    svr.Post("/api/posts", [&](const httplib::Request, &req, httplib::Response& res){
+        decltype(auto) content = req.params.find("content");
+        Post post{-1, content};
+        storage.insert(post);
+    })
 
     svr.listen("localhost", 3000);
 }
